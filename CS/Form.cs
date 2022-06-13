2e3b09faca44a62c708ab2036d2d550d383ac5b7
bhokaresneha <bhokaresneha20@gmail.com>
2022-06-13 14:34:42 +0530

    Validating Password Rules

---
+++ CS/Form.cs
@@ -0,0 +1,46 @@
+html, input { 
+    font-family: sans-serif;
+}
+body {
+    padding:5%;
+    width: 100%; 
+    max-width: 600px; 
+    margin: 0px auto; 
+    background-image:url('https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000');
+}
+.headerContainer { 
+    display: flex;
+    flex-flow: column wrap; 
+    align-content: center;
+    background-color:#008CFF ; 
+    font-weight: bold; margin-bottom: 20px;
+}
+form {
+    margin-top: auto;
+}
+div {
+    display: flex;
+    margin-bottom: 20px; 
+    justify-content: space-between; 
+    align-items: center;
+}
+label, input, button { 
+    font-size: 16px; 
+    line-height: 1.5
+}
+label {
+   
+   
+    width: 30%; 
+    margin-right: 2%;
+}
+input {
+    flex: auto;
+}
+button {
+    width: 70%; margin: 0 auto;
+}
+error-output {
+    font-size: 12px;
+    font-style: italic;
+    color: red;}
---
+++ JS/Form.js
@@ -0,0 +1,37 @@
+// Code For Salary
+const salary = document.querySelector('#salary');
+        const output = document.querySelector('.salary-output');
+        output.textContent = salary.value;
+        salary.addEventListener('input', function () {
+            output.textContent = salary.value;
+        });
+
+
+// Validation For Name
+const text= document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input',function(){
+    let nameRgx = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRgx.test(text.value))
+    textError.textContent= "";
+    else textError.textContent = "Name Is Incorrect";
+});
+
+// Validation for Phone Number
+const tel = document.querySelector('#tel');
+const telError = document.querySelector('.tel-error');
+tel.addEventListener('input', function(){
+    let telRgx = RegExp('^[0-9]{2}\\s{0,1}[0-9]{10}$');
+    if(telRgx.test(tel.value))
+    telError.textContent= "";
+    else telError.textContent = "Phone No Is Incorrect";
+});
+//Validating Password
+const pwd = document.querySelector('#pwd');
+const pwdError = document.querySelector('.pwd-error');
+pwd.addEventListener('input', function(){
+    let pwdRgx = RegExp('^(?=.*[A-Z])(?=.*[a-z0-9])(?=.*[@#$%^&-+=()]).{8}$');
+    if(pwdRgx.test(pwd.value))
+    pwdError.textContent= "";
+    else pwdError.textContent = "Password Is Incorrect";
+});
